c9e2e24e79777a15759aafcbeb48b3eb61b54349
CKylinMC <Cansll@users.noreply.github.com>
2019-06-19 23:38:13 +0800

    增加 虚空商人功能
    未完成商人背包功能

--- Telebot.js
+++ Telebot.js
@@ -1,7 +1,9 @@
 var token = 'xxxx';
 var statapi = 'https://api.warframestat.us/pc';
 var nightwaveDict = "https://raw.githubusercontent.com/Richasy/WFA_Lexicon/master/WF_NightWave.json";
+var wfDict = "https://raw.githubusercontent.com/Richasy/WFA_Lexicon/master/WF_Dict.json";
 var showMsgId = false;
+var showWaitMsg = false;
 
 
 function doPost(e) {
@@ -19,12 +21,19 @@ function doPost(e) {
   bus.on(/\/help/, getStarted);
   bus.on(/\/帮助/, getStarted);
    
+  bus.on(/\/时间\s*([A-Za-z0–9_]+)?\s*([A-Za-z0–9_]+)?/, getCycles);
   bus.on(/\/时间播报\s*([A-Za-z0–9_]+)?\s*([A-Za-z0–9_]+)?/, getCycles);
   bus.on(/\/time\s*([A-Za-z0–9_]+)?\s*([A-Za-z0–9_]+)?/, getCycles);
    
+  bus.on(/\/夜波\s*([A-Za-z0–9_]+)?\s*([A-Za-z0–9_]+)?/, getNightwaves);
   bus.on(/\/午夜电波\s*([A-Za-z0–9_]+)?\s*([A-Za-z0–9_]+)?/, getNightwaves);
   bus.on(/\/nightwave\s*([A-Za-z0–9_]+)?\s*([A-Za-z0–9_]+)?/, getNightwaves);
   bus.on(/\/nw\s*([A-Za-z0–9_]+)?\s*([A-Za-z0–9_]+)?/, getNightwaves);
+   
+  bus.on(/\/奸商\s*([A-Za-z0–9_]+)?\s*([A-Za-z0–9_]+)?/, getVoidTrager);
+  bus.on(/\/虚空商人\s*([A-Za-z0–9_]+)?\s*([A-Za-z0–9_]+)?/, getVoidTrager);
+  bus.on(/\/voidtrader\s*([A-Za-z0–9_]+)?\s*([A-Za-z0–9_]+)?/, getVoidTrager);
+  bus.on(/\/vd\s*([A-Za-z0–9_]+)?\s*([A-Za-z0–9_]+)?/, getVoidTrager);
    
   bot.register(bus);
  
@@ -139,6 +148,7 @@ function getStarted(){
     +"/help | /帮助 : 查询帮助(此消息)。\n"
     +"/time | /时间播报 : 查询平原和地球的时间和气候。\n"
     +"/nw | /午夜电波 : 查询午夜电波任务信息。\n"
+    +"/vd | /虚空商人 : 查询虚空商人信息(未完成)。\n"
     +"\n\nBot developed by ShadowKylin氏族\n"
     +"API: WarframeStat.us\n"
     +"DICT: https://github.com/Richasy/WFA_Lexicon"
@@ -157,10 +167,14 @@ function getBJTime(t){
     return d;
 }
 
+function parseTime(t){
+  return t.replace("d","天").replace("h","小时").replace("m","分钟").replace("s","秒");
+}
+
 //////////////////////////////////////////// Cycles
 
 function getCycles(){
-  this.replyToSender("正在获取数据...");
+  if(showWaitMsg)this.replyToSender("正在获取数据...");
   var data = getStat();
   var d = getBJTime(data.timestamp);
   var msg = "时间播报:\n\n"
@@ -168,13 +182,13 @@ function getCycles(){
     +d.getUTCFullYear()+"年"+(d.getUTCMonth()+1)+"月"+d.getUTCDate()+"日"+d.getUTCHours()+":"+d.getUTCMinutes()+":"+d.getUTCSeconds()+"\n\n"
     +"*地球*\n"
     +"- 当前："+(data.earthCycle.isDay?"白天":"夜晚")+"\n"
-    +"- 距离切换："+data.earthCycle.timeLeft+"\n\n"
+    +"- 距离切换："+parseTime(data.earthCycle.timeLeft)+"\n\n"
     +"*希图斯*\n"
     +"- 当前："+(data.cetusCycle.isDay?"白天":"夜晚")+"\n"
-    +"- 距离切换："+data.cetusCycle.timeLeft+"\n\n"
+    +"- 距离切换："+parseTime(data.cetusCycle.timeLeft)+"\n\n"
     +"*福尔图纳*\n"
     +"- 当前："+(data.vallisCycle.isWarm?"温暖":"寒冷")+"\n"
-    +"- 距离切换："+data.vallisCycle.timeLeft+"\n\n";
+    +"- 距离切换："+parseTime(data.vallisCycle.timeLeft)+"\n\n";
   this.replyToSender(msg);
 }
 
@@ -185,7 +199,7 @@ function getNWDict(){
 }
 
 function getNightwaves(){
-  this.replyToSender("正在获取数据...");
+  if(showWaitMsg)this.replyToSender("正在获取数据...");
   var dict = getNWDict();
   var data = getStat();
   var nwc = data.nightwave.activeChallenges;
@@ -204,5 +218,45 @@ function getNightwaves(){
   }else
     contents+= "未开放。";
   
+  this.replyToSender(contents);
+}
+
+//////////////////////////////////////////// Baro Ki Teer
+
+function getWFDict(){
+  return JSON.parse(UrlFetchApp.fetch(wfDict).getContentText());;
+}
+
+function getVTPoint(p,dict){
+  var r = /^.+?\((.+?)\).*$/;
+  var m = r.exec(p);
+  if(m){
+    if(!dict) dict = getWFDict();
+    var n = dict.filter(function(a){return a.en==m[1]})[0].zh
+    if(n) return n;
+    else return m[1];
+  }
+  else return p;
+}
+
+function getVoidTrager(){
+  if(showWaitMsg)this.replyToSender("正在获取数据...");
+  var data = getStat();
+  var dict = getWFDict();
+  var vd = data.voidTrader
+  var contents = "*虚空商人*("+(vd.active?"已到达":"未到达")+")";
+  contents+= "\n\n目标节点："+getVTPoint(vd.location,dict)+"中继站";
+  if(vd.active){
+    var d = getBJTime(vd.expiry);
+    contents+= "\n\n离开时间：\n"
+    +d.getUTCFullYear()+"年"+(d.getUTCMonth()+1)+"月"+d.getUTCDate()+"日"+d.getUTCHours()+":"+d.getUTCMinutes()+":"+d.getUTCSeconds()
+    +"\n("+parseTime(vd.endString)+" 后)";
+    contents+= "\n\n携带物品：\n(开发中...)";
+  }else{
+    var d = getBJTime(vd.activation);
+    contents+= "\n\n到达时间：\n"
+    +d.getUTCFullYear()+"年"+(d.getUTCMonth()+1)+"月"+d.getUTCDate()+"日"+d.getUTCHours()+":"+d.getUTCMinutes()+":"+d.getUTCSeconds()
+    +"\n("+parseTime(vd.startString)+" 后)";
+  }
   this.replyToSender(contents);
 }
